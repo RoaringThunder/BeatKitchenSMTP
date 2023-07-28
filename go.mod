55eefdb4363946679f78c262fa0151528ddf4eb6
EOIN HOLLAND <eoinhwork@gmail.com>
2023-07-28 03:54:39 +0100

    chnage to tls port and use gomail

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module salamander-smtp
 go 1.20
 
 require (
+	github.com/go-mail/mail v2.3.1+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
@@ -12,6 +13,7 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gorm.io/driver/mysql v1.5.1 // indirect
 	gorm.io/driver/postgres v1.5.2 // indirect
 	gorm.io/gorm v1.25.1 // indirect
--- service/service.go
+++ service/service.go
@@ -1,26 +1,34 @@
 package service
 
 import (
-	"net/smtp"
 	"os"
+	"strconv"
+
+	"github.com/go-mail/mail"
 )
 
 func SendEmail(to []string, html string) error {
 	username := os.Getenv("SMTP_USERNAME")
 	secret := os.Getenv("SMTP_SECRET")
 	host := os.Getenv("SMTP_HOST")
-	port := os.Getenv("SMTP_PORT")
-	from := os.Getenv("SMTP_FROM")
-	addr := host + ":" + port
 
-	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
-	subject := "Subject: " + "Verify email" + "!\n" + "To: " + to[0] + "\n"
-	msg := []byte(subject + mime + "\n" + html)
-
-	auth := smtp.CRAMMD5Auth(username, secret)
-
-	err := smtp.SendMail(addr, auth, from, to, msg)
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
 	if err != nil {
+		return err
+	}
+	from := os.Getenv("SMTP_FROM")
+
+	m := mail.NewMessage()
+
+	m.SetHeader("From", from)
+	m.SetHeader("To", to...)
+	m.SetHeader("Subject", "Verify Email!")
+	m.SetBody("text/html", html)
+
+	d := mail.NewDialer(host, port, username, secret)
+
+	// Send the email to recipients.
+	if err := d.DialAndSend(m); err != nil {
 		return err
 	}
 
